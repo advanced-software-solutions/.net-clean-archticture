8ee69cae20b6fcb3e83b21828ae9e1c550e978f3
Amr Swalha <amr.swalha.code@outlook.com>
2024-12-10 20:41:10 +0300

    final work for generic calls

--- CleanAPI/Controllers/GenericController.cs
+++ CleanAPI/Controllers/GenericController.cs
@@ -3,7 +3,7 @@ using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace CleanAPI.Controllers
 {
@@ -11,13 +11,11 @@ namespace CleanAPI.Controllers
     [Route("[controller]/[action]")]
     public class GenericController(IConfiguration configuration) : ControllerBase
     {
-        /// <summary>
-        /// PLEASE BE CAREFUL WHEN USING THIS
-        /// it's an initial work, use it at your own risk
-        /// </summary>
         [HttpPost]
         public IActionResult Get(QueryGeneric query)
         {
+            CheckForSqlInjection check = new();
+            check.IsSafe(query);
             using (SqlConnection sql = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
             {
                 string queryString = $"SELECT * from {query.Table}";
@@ -47,6 +45,8 @@ namespace CleanAPI.Controllers
         [HttpPost]
         public IActionResult Insert(InsertGeneric insert)
         {
+            CheckForSqlInjection check = new();
+            check.IsSafe(insert);
             string query = $"INSERT INTO {insert.Table} \n\r(";
             query += $"" + string.Join(',', insert.Data.Select(r => $"[{r.Key.Replace(" ", "").Replace("-", "")}]").ToArray()) + ")\n\r";
             query += $"VALUES \n\r(" + string.Join(',', insert.Data.Select(r => $"@{r.Key}").ToArray()) + ")";
@@ -61,8 +61,10 @@ namespace CleanAPI.Controllers
         }
 
         [HttpPut]
-        public IActionResult Update(UpdateGeneric update) 
+        public IActionResult Update(UpdateGeneric update)
         {
+            CheckForSqlInjection check = new();
+            check.IsSafe(update);
             string query = $"UPDATE {update.Table} \n\r";
             query += $"SET " + update.Data.ToSqlParamters() + "\n\r";
             if (update.Values?.Count > 0)
@@ -83,6 +85,20 @@ namespace CleanAPI.Controllers
             var effectedRows = sql.Execute(query, new DynamicParameters(parametersQuery));
             return Ok(effectedRows);
         }
+
+        [HttpPost]
+        public IActionResult Delete(DeleteGeneric query)
+        {
+            CheckForSqlInjection check = new();
+            check.IsSafe(query);
+            using (SqlConnection sql = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                string queryString = $"DELETE from {query.Table}";
+                queryString += $"\r\nWHERE {string.Join(" and ", query.Data.Select(r => $"[{r.Key.Replace(" ", "").Replace("-", "")}]={r.Value}").ToArray())}";
+                var result = sql.Execute(queryString);
+                return new JsonResult(result);
+            }
+        }
     }
 
     public class QueryGeneric
@@ -98,11 +114,78 @@ namespace CleanAPI.Controllers
     {
         public string Table { get; set; }
         public Dictionary<string, string> Data { get; set; } = new();
-    } 
+    }
     public class UpdateGeneric
     {
         public string Table { get; set; }
         public Dictionary<string, string> Data { get; set; } = new();
         public Dictionary<string, string> Values { get; set; } = new();
+    }
+    public class DeleteGeneric
+    {
+        public string Table { get; set; }
+        public Dictionary<string, string> Data { get; set; } = new();
+    }
+    public class CheckForSqlInjection
+    {
+        public List<string> NoPass = new List<string>()
+        {
+            "delete",
+            "drop",
+            "insert",
+            "create"
+        };
+
+
+        public void IsSafe(QueryGeneric parameter)
+        {
+            foreach (var item in NoPass)
+            {
+                if (MatchExact(parameter.Table, item))
+                    throw new Exception("Not safe sql");
+                if (parameter.Where != null && MatchExact(parameter.Where, item))
+                    throw new Exception("Not safe sql");
+                if (!string.IsNullOrEmpty(parameter.OrderBy) && MatchExact(parameter.OrderBy, item))
+                    throw new Exception("Not safe sql");
+                if (parameter.Columns?.Length > 0)
+                {
+                    for (int i = 0; i < parameter.Columns.Length; i++)
+                    {
+                        if (MatchExact(parameter.Columns[i], item))
+                            throw new Exception("Not safe sql");
+                    }
+                }
+            }
+        }
+
+        public void IsSafe(InsertGeneric parameter)
+        {
+            foreach (var item in NoPass)
+            {
+                if (MatchExact(parameter.Table, item))
+                    throw new Exception("Not safe sql");
+            }
+        }
+        public void IsSafe(UpdateGeneric parameter)
+        {
+            foreach (var item in NoPass)
+            {
+                if (MatchExact(parameter.Table, item))
+                    throw new Exception("Not safe sql");
+            }
+        }
+        public void IsSafe(DeleteGeneric parameter)
+        {
+            foreach (var item in NoPass)
+            {
+                if (MatchExact(parameter.Table, item))
+                    throw new Exception("Not safe sql");
+            }
+        }
+        public bool MatchExact(string word, string match)
+        {
+            return Regex.IsMatch(word.Trim().ToLower(), @$"\b{match}\b");
+        }
+
     }
 }
