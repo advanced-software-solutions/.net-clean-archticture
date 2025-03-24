559dca95ca2852b5a15129020ec81145512e9652
Amr Swalha <amr.swalha.code@outlook.com>
2025-03-25 02:51:23 +0300

    more work on actor model

--- CleanBase/Entities/TodoList.cs
+++ CleanBase/Entities/TodoList.cs
@@ -1,9 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
+﻿namespace CleanBase.Entities;
 
-namespace CleanBase.Entities;
-
-[Table("[dbo].[TodoList]")]
 public partial class TodoList : EntityRoot
 {
     public string Title { get; set; }
